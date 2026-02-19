48af4966a25977e3bb9d12a43809af312c9f0666
emelielundvall <luem25zf@student.ju.se>
2026-02-19 11:34:56 +0100

    cart

--- js/script.js
+++ js/script.js
@@ -1,12 +1,31 @@
+
+// quantity //
 const quantitySelectors = document.querySelectorAll('.quantity-selector');
+const totalElement = document.querySelector('.total-price');
+
+function updateTotal() {
+    let total = 0;
+
+    quantitySelectors.forEach(selector => {
+    
+        const quantityInput = selector.querySelector('.quantity');
+        const priceElement = selector.closest('.cart-items').querySelector('.price');
+
+        const price = parseFloat(priceElement.dataset.price);
+        const quantity = parseInt(quantityInput.value);
+
+        total += price * quantity;
+    });
+
+    totalElement.textContent = total.toFixed(2) + " €";
+}
 
 quantitySelectors.forEach(selector => {
-    
+
     const decreaseBtn = selector.querySelector('.decrease');
     const increaseBtn = selector.querySelector('.increase');
     const quantityInput = selector.querySelector('.quantity');
 
-    
     if (decreaseBtn && increaseBtn && quantityInput) {
         
         // decrease
@@ -14,6 +33,7 @@ quantitySelectors.forEach(selector => {
             let currentValue = parseInt(quantityInput.value);
             if (currentValue > 1) {
                 quantityInput.value = currentValue - 1;
+                updateTotal();
             }
         });
 
@@ -21,6 +41,15 @@ quantitySelectors.forEach(selector => {
         increaseBtn.addEventListener('click', () => {
             let currentValue = parseInt(quantityInput.value);
             quantityInput.value = currentValue + 1;
+            updateTotal();
         });
     }
 });
+
+updateTotal();
+
+
+    
+
+    
+    
